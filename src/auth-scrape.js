86da9cb7af265707426973e0cf967d03c61746fc
Jonas Mittnacht <cookie.mijo@gmail.com>
2023-01-22 02:04:22 +0100

    very little progress

--- src/auth-scrape.js
+++ src/auth-scrape.js
@@ -1,13 +1,20 @@
 var $ = require('jquery');
 
 async function makeLoginRequest() {
-    /*var response = await fetch('https://ilias.studium.kit.edu/shib_login.php?target=root_1');
-    let url = response.url;*/
+    let response = await fetch('https://ilias.studium.kit.edu/shib_login.php?target=root_1');
+    let url = response.url;
+    let csrfToken = getCSRFToken(await response.text());
+
 
     setTimeout(() => redirectBack(), 2000);
 }
 
-async function redirectBack() {
+function getCSRFToken(domString) {
+    let value = $('input[name="csrf_token"]', $(domString)).val();
+    return value;
+}
+
+function redirectBack() {
     var url = new URL(window.location.href);
     var redirectTo = url.searchParams.get('redirect_to');
 
