11f66b69e3058e26dd0e5eaabf4c1499b8de0ee4
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2024-10-17 17:39:31 +0200

    Readd support for FELS logins

--- src/authenticator/authenticators.js
+++ src/authenticator/authenticators.js
@@ -2,6 +2,14 @@ const browser = require('webextension-polyfill');
 const { getConfig } = require('../common/config');
 const { getLoginDetector } = require('../common/login_detectors');
 
+const domParser = new DOMParser();
+
+const loginFormPostHeaders = {
+    postHeaders: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+}
+
 class DefaultAuthenticator {
     #name;
     #pageId;
@@ -69,8 +77,131 @@ class DefaultAuthenticator {
     }
 }
 
+class FELSAuthenticator extends DefaultAuthenticator {
+    static LOGIN_PAGE = "https://fels.scc.kit.edu/Shibboleth.sso/Login";
+    static FELS_FORM_SELECT_KIT = {
+        'jakarta.faces.partial.ajax': 'true',
+        'jakarta.faces.source': 'searchAutocompl',
+        'jakarta.faces.partial.execute': 'searchAutocompl',
+        'jakarta.faces.partial.render': 'form',
+        'jakarta.faces.behavior.event': 'valueChange',
+        'jakarta.faces.partial.event': 'change',
+        'form': 'form',
+        'searchAutocompl_input': 'Karlsruher+Institut+für+Technologie+(KIT)',
+        'searchAutocompl_hinput': '1002'
+    };
+    static FELS_FORM_SEND_KIT = {
+        'jakarta.faces.partial.ajax': 'true',
+        'jakarta.faces.source': 'login',
+        'jakarta.faces.partial.execute': '@all',
+        'jakarta.faces.partial.render': 'form',
+        'login': 'login',
+        'form': 'form',
+        'searchAutocompl_input': 'Karlsruher+Institut+für+Technologie+(KIT)',
+        'searchAutocompl_hinput': '1002'
+    }
+
+    static VIEWSTATE_FIELD = "jakarta.faces.ViewState";
+
+    async authenticate(pageUrl) {
+        let signInPageResponse = await this.initiateAuthentication(pageUrl);
+        if (signInPageResponse.ok && new URL(signInPageResponse.url).hostname != new URL(FELSAuthenticator.LOGIN_PAGE).hostname) {
+            return signInPageResponse;
+        }
+
+        await this.selectKITOnFELSPage(signInPageResponse);
+        
+        return await super.authenticate(FELSAuthenticator.LOGIN_PAGE);
+    }
+
+    async initiateAuthentication(pageUrl) {
+        console.log(`loading login page from ${pageUrl}`);
+        let pageLoadResponse = await fetch(pageUrl, {
+            method: 'GET'
+        });
+
+        let document = await parseResponseToDoc(pageLoadResponse);
+
+        return await this.autoSubmitForm(pageUrl, document);
+    }
+
+    async selectKITOnFELSPage(response) {
+        console.log(`selecting KIT on ${response.url}`);
+
+        let doc = await parseResponseToDoc(response);
+        let formData = this.fillFELSSelectionForm(doc, FELSAuthenticator.FELS_FORM_SELECT_KIT);
+        const selectResponse = await fetch(response.url, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const sendFormData = this.fillFELSSelectionForm(doc, FELSAuthenticator.FELS_FORM_SEND_KIT);
+
+        return await fetch(selectResponse.url, {
+            method: 'POST',
+            body: sendFormData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+    }
+
+    fillFELSSelectionForm(doc, defaults) {
+        let formData = new URLSearchParams();
+
+        for (let fieldName in defaults) {
+            formData.append(fieldName, defaults[fieldName]);
+        }
+
+        let viewState = doc.querySelector(`input[name="${FELSAuthenticator.VIEWSTATE_FIELD}"]`);
+        formData.append(FELSAuthenticator.VIEWSTATE_FIELD, viewState.value);
+
+        return formData;
+    }
+
+    async autoSubmitForm(originalUrl, doc) {
+        let form = doc.forms[0];
+
+        let formData = this.scrapePresetFormDetails(doc, 0);
+        let responseURL = new URL(originalUrl);
+        let url = new URL(form.getAttribute('action'), responseURL.origin + responseURL.pathname).toString();
+
+        console.log(`auto-submitting form from ${originalUrl} to ${url}`);
+        return await fetch(url, {
+            method: 'POST',
+            headers: loginFormPostHeaders,
+            body: formData
+        });
+    }
+
+    scrapePresetFormDetails(doc, formId) {
+        let formData = new URLSearchParams();
+
+        let form = doc.forms[formId];
+        let elementsCollection = form.getElementsByTagName('input');
+        for (let i = 0; i < elementsCollection.length; i++) {
+            let element = elementsCollection.item(i);
+            formData.append(element.name, element.value);
+        }
+
+        return formData;
+    }
+}
+
+async function parseResponseToDoc(response) {
+    return domParser.parseFromString(await response.text(), 'text/html');
+}
+
 function getAuthenticator(type, pageId) {
-    return new DefaultAuthenticator(type, pageId);
+    switch (type) {
+        case 'fels':
+            return new FELSAuthenticator(type, pageId);
+        default:
+            return new DefaultAuthenticator(type, pageId);
+    }
 }
 
 module.exports = {
